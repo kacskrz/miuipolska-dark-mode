f8e2ed9c48253263723df212c9e99b2eb5c067d3
Orjon <kacskrz@gmail.com>
2018-08-28 19:13:30 +0200

    initial

---
+++ README.md
@@ -0,0 +1,3 @@
+# MIUIPolska dark mode
+Script for forum & website MIUIPolska.pl.
+For now - available only for forum, for website will available soon. :)
---
+++ script.js
@@ -0,0 +1,41 @@
+// ==UserScript==
+// @name         Forum MIUIPolska.pl - czarny tryb
+// @namespace    https://github.com/kacskrz/miuipolska-dark-mode
+// @version      1.0
+// @description  Czarny motyw dla forum MIUIPolska.pl
+// @author       Orjon (kacskrz@gmail.com)
+// @match        *://miuipolska.pl/forum/*
+// @grant        GM_addStyle
+// ==/UserScript==
+GM_addStyle('body { background-color: #272222; color: #aba3a3;');
+GM_addStyle('#ipsLayout_footer a, #ipsLayout_footer p { color: #dbdee0;');
+GM_addStyle('.ipsBreadcrumb [data-action="defaultStream"], .ipsBreadcrumb [data-action="markSiteRead"] { color: #f0f1f3;');
+GM_addStyle('.fa { color: #fff;');
+GM_addStyle('.ipsBreadcrumb > ul > li > a { color: floralwhite;');
+GM_addStyle('.ipsAreaBackground_reset { background: #2f2e2e;');
+GM_addStyle('a { color: #dee1e2;');
+GM_addStyle('.ipsStreamItem_snippet .ipsType_richText { color: #b3b1b1;');
+GM_addStyle('.ipsNavBar_secondary > li.ipsNavBar_active a { color: #d5d8da;');
+GM_addStyle('.ipsNavBar_secondary > li > a { color: rgba(203, 205, 206, 0.63);');
+GM_addStyle('.ipsType_pageTitle { color: #e0dddd;');
+GM_addStyle('.ipsNavBar_secondary { background: #272727;');
+GM_addStyle('#ipsLayout_header nav { background: #444444;');
+GM_addStyle('.ipsBox:not( .ipsBox_transparent ):not( .ipsModerated ) { background-color: #211f1f;');
+GM_addStyle('.ipsNavBar_primary:not( .ipsNavBar_noSubBars ) > ul:before { background: #272727;');
+GM_addStyle('.ipsComment:not( .ipsModerated ) .ipsComment_header { background: #424242; border-top: 1px solid #4b4c4c;');
+GM_addStyle('.ipsComment { background: #333232;');
+GM_addStyle('.ipsType_richText { color: #dee1e2;');
+GM_addStyle('.ipsApp .ipsButton_light { background: #737070; color: white;');
+GM_addStyle('#cke_1_contents > div { background-color: #635e5e;');
+GM_addStyle('.ipsAttachment_supportDrag { color: black;');
+GM_addStyle('.cMessage_active { background: #434e54 !important;');
+GM_addStyle('.ipsDataList.ipsDataList_zebra .ipsDataItem:not( .ipsDataItem_selected ):not( .ipsModerated ):not( .ipsDataItem_new ):not( .ipsDataItem_success ):not( .ipsDataItem_warning ):not( .ipsDataItem_error ):not( .ipsDataItem_info ):not( .ipsDataItem_status ):nth-child(even) { background: #3e3d3d;');
+GM_addStyle('.ipsDataItem_unread { background: #423737;');
+GM_addStyle('.ipsDataList_readStatus .ipsDataItem:not( .ipsDataItem_unread ):not( .ipsDataItem_selected ):not( .ipsModerated ) { background: #544343;');
+GM_addStyle('.ipsMenu_headerBar, .ipsMenu_footerBar { background: #696868;');
+GM_addStyle('.ipsType_sectionHead { color: #ded3d3;');
+GM_addStyle('html[dir="ltr"] .cMessage { background: #544343;');
+GM_addStyle('.ipsNavBar_secondary > li > a:hover { color: rgba(128, 136, 142, 0.8);');
+GM_addStyle('.ipsBreadcrumb > ul > li { color: #e8dcdc;');
+GM_addStyle('.ipsSpoiler, .ipsStyle_spoiler { background: #504c4c;');
+GM_addStyle('.ipsQuote { background: #4a4747;');
